523b5590590afe67287501e1fefdea9dac3df82e
Sundeep Goel <sundeepgoel2@gmail.com>
2026-05-29 13:24:24 +0530

    Fix DWIN display registration for external component compile

--- esphome/components/dwin/__init__.py
+++ esphome/components/dwin/__init__.py
@@ -1,8 +1,9 @@
 import esphome.codegen as cg
+from esphome.components import display, uart
 
 CODEOWNERS = ["@sundeepgoel72"]
 DEPENDENCIES = ["uart"]
 
 dwin_ns = cg.esphome_ns.namespace("dwin")
-DWIN = dwin_ns.class_("DWIN")
+DWIN = dwin_ns.class_("DWIN", cg.PollingComponent, uart.UARTDevice, display.DisplayBuffer)
 dwin_ref = DWIN.operator("ref")
